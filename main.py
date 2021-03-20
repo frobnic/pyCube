be8b8f8d29b5370bc24422469a0bcbd4230a73c0
frank paulsen <frobnic@gmail.com>
2021-03-20 12:10:49 +0100

    Adding many cubes

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-######
 import random
 
-verticies = (
+vertices = (
     (1, -1, -1),
     (1, 1, -1),
     (-1, 1, -1),
@@ -61,7 +60,30 @@ colors = (
 )
 
 
-def Cube():
+def set_vertices(max_distance):
+    x_value_change = random.randrange(-10, 10)
+    y_value_change = random.randrange(-10, 10)
+    z_value_change = random.randrange(-1 * max_distance, -20)
+
+    new_vertices = []
+
+    for vert in vertices:
+        new_vert = []
+
+        new_x = vert[0] + x_value_change
+        new_y = vert[1] + y_value_change
+        new_z = vert[2] + z_value_change
+
+        new_vert.append(new_x)
+        new_vert.append(new_y)
+        new_vert.append(new_z)
+
+        new_vertices.append(new_vert)
+
+    return new_vertices
+
+
+def Cube(vertices):
     glBegin(GL_QUADS)
 
     for surface in surfaces:
@@ -70,14 +92,14 @@ def Cube():
         for vertex in surface:
             x += 1
             glColor3fv(colors[x])
-            glVertex3fv(verticies[vertex])
+            glVertex3fv(vertices[vertex])
 
     glEnd()
 
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
-            glVertex3fv(verticies[vertex])
+            glVertex3fv(vertices[vertex])
     glEnd()
 
 
@@ -88,15 +110,23 @@ def main():
 
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
-    # start further back
-    glTranslatef(random.randrange(-5, 5), 0, -30)
+    glTranslatef(random.randrange(-5, 5), random.randrange(-5, 5), -40)
 
-    # no more rotate
+    # object_passed = False
+
+    x_move = 0
+    y_move = 0
+
+    max_distance = 100
+
+    cube_dict = {}
+
+    for x in range(20):
+        cube_dict[x] = set_vertices(max_distance)
+
     # glRotatef(25, 2, 1, 0)
 
-    object_passed = False
-
-    while not object_passed:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -104,42 +134,50 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    glTranslatef(-0.5, 0, 0)
+                    x_move = 0.3
                 if event.key == pygame.K_RIGHT:
-                    glTranslatef(0.5, 0, 0)
+                    x_move = -0.3
 
                 if event.key == pygame.K_UP:
-                    glTranslatef(0, 1, 0)
+                    y_move = -0.3
                 if event.key == pygame.K_DOWN:
-                    glTranslatef(0, -1, 0)
-            '''
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 4:
-                    glTranslatef(0,0,1.0)
+                    y_move = 0.3
 
-                if event.button == 5:
-                    glTranslatef(0,0,-1.0)
-            '''
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_move = 0
 
-        x = glGetDoublev(GL_MODELVIEW_MATRIX)  # , modelviewMatrix)
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_move = 0
+
+        ##            if event.type == pygame.MOUSEBUTTONDOWN:
+        ##                if event.button == 4:
+        ##                    glTranslatef(0,0,1.0)
+        ##
+        ##                if event.button == 5:
+        ##                    glTranslatef(0,0,-1.0)
+
+        # glRotatef(1, 3, 1, 1)
+
+        x = glGetDoublev(GL_MODELVIEW_MATRIX)
+        # print(x)
 
         camera_x = x[3][0]
         camera_y = x[3][1]
         camera_z = x[3][2]
 
-        # print(camera_x,camera_y,camera_z)
-
-        # slowly move:
-        glTranslatef(0, 0, 0.5)
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        Cube()
+
+        glTranslatef(x_move, y_move, .50)
+
+        for each_cube in cube_dict:
+            Cube(cube_dict[each_cube])
+
         pygame.display.flip()
-
-        if camera_z <= 0:
-            object_passed = True
+        pygame.time.wait(10)
 
 
-for x in range(10000):
-    main()
+main()
+pygame.quit()
+quit()
 
